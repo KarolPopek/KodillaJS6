3c148d578bd0a941381904da0cfb7b5ce1ad06b1
KarolPopek <karol.popek@wp.pl>
2017-02-22 11:25:54 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+var femaleNames = [ 'Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+
+console.log(femaleNames);
+console.log(maleNames);
+
+var allNames = femaleNames.concat(maleNames);
+
+console.log(allNames);
+
+var newName = 'Marian';
+
+if ( allNames.indexOf(newName) == -1 ) {
+	allNames.push(newName);
+}
+
+console.log(allNames);
